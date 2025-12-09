0c8393f58b8319b9503029cb90a5fe1ec8d9aaff
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-12-08 22:50:27 -0500

    Small change

--- Stages/FightZone/fight_zone.gd
+++ Stages/FightZone/fight_zone.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready() -> void:
-	var spaceship = preload("res://Entities/Spaceship/spaceship.tscn").instantiate()
+	var spaceship = preload(
+		"res://Entities/Spaceship/spaceship.tscn").instantiate()
 	spaceship.position = Vector2(1920,1080)
 	add_child(spaceship)
 	
