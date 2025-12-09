d17b07e2a66a082a56199e967b59998439b94b75
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-12-08 22:48:10 -0500

    Added spaceship trails

--- Entities/Spaceship/spaceship.gd
+++ Entities/Spaceship/spaceship.gd
@@ -4,8 +4,9 @@ func _physics_process(delta: float) -> void:
 	var jv = Input.get_vector("JLD0","JRD0","JUD0","JDD0")
 	if (jv.length() > 0.3): 
 		var d =  Vector2(0,-1).rotated(rotation).cross(jv.normalized())
-		if (d > 0.1):
+		if (d > 0.01):
 			rotation += PI * delta
-		if (d < 0.1):
+		if (d < 0.01):
 			rotation -= PI * delta
+			
 	position +=  Vector2(0,-750).rotated(rotation) * delta
--- Stages/FightZone/fight_zone.gd
+++ Stages/FightZone/fight_zone.gd
@@ -4,3 +4,19 @@ func _ready() -> void:
 	var spaceship = preload("res://Entities/Spaceship/spaceship.tscn").instantiate()
 	spaceship.position = Vector2(1920,1080)
 	add_child(spaceship)
+	
+	var spaceship_trail_1 = preload(
+		"res://Entities/SpaceshipTrail/spaceship_trail.tscn").instantiate()
+	spaceship_trail_1.spaceship_instance_id = spaceship.get_instance_id()
+	spaceship_trail_1.spaceship_engine = 0
+	spaceship_trail_1.default_color = Color(0,255,255,1)
+	add_child(spaceship_trail_1)
+	
+	var spaceship_trail_2 = preload(
+		"res://Entities/SpaceshipTrail/spaceship_trail.tscn").instantiate()
+	spaceship_trail_2.spaceship_instance_id = spaceship.get_instance_id()
+	spaceship_trail_2.spaceship_engine = 1
+	spaceship_trail_2.default_color = Color(0,255,255,1)
+	
+	add_child(spaceship_trail_2)
+	
