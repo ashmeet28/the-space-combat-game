085374a209edd26c558260761d2c44ce16215997
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-12-16 22:32:59 -0500

    Added controller inputs

--- Entities/Spaceship/spaceship.gd
+++ Entities/Spaceship/spaceship.gd
@@ -2,9 +2,17 @@ extends Area2D
 
 var ROTATION_SPEED = PI
 var DEFAULT_SPEED = 750
+var controller_input_map = []
 
 func _physics_process(delta: float) -> void:
-	var jv = Input.get_vector("JLD0","JRD0","JUD0","JDD0")
+	if len(controller_input_map) == 0:
+		return
+
+	var jv = Input.get_vector(controller_input_map[0],
+	controller_input_map[1],
+	controller_input_map[2],
+	controller_input_map[3])
+	
 	if (jv.length() > 0): 
 		var d =  Vector2.UP.rotated(rotation).cross(jv.normalized())
 		if (d > 0.001):
@@ -13,3 +21,7 @@ func _physics_process(delta: float) -> void:
 			rotation -= ROTATION_SPEED * delta
 			
 	position +=  (Vector2.UP * DEFAULT_SPEED * delta).rotated(rotation)
+	
+	for a in controller_input_map:
+		if Input.is_action_just_pressed(a):
+			print(a)
--- Stages/FightZone/fight_zone.gd
+++ Stages/FightZone/fight_zone.gd
@@ -1,17 +1,27 @@
 extends Node2D
 
+var controller_inputs = [
+	["JA0LD0","JA0RD0","JA1UD0","JA1DD0","JB0D0",
+	"JB0D0","JB1D0","JB2D0","JB3D0",
+	"JB4D0","JB5D0","JB6D0","JB7D0",
+	"JB8D0","JB9D0","JB10D0","JB11D0",
+	"JB12D0","JB13D0","JB14D0","JB15D0",
+	"JB16D0","JB17D0","JB18D0","JB19D0",
+	"JB20D0"]]
+
 func _ready() -> void:
 	var spaceship = preload(
 		"res://Entities/Spaceship/spaceship.tscn").instantiate()
 	spaceship.position = Vector2(1920,1080)
-	add_child(spaceship)
+	spaceship.controller_input_map = controller_inputs[0]
+	add_child.call_deferred(spaceship)
 	
 	var spaceship_trail_1 = preload(
 		"res://Entities/SpaceshipTrail/spaceship_trail.tscn").instantiate()
 	spaceship_trail_1.spaceship_instance_id = spaceship.get_instance_id()
 	spaceship_trail_1.spaceship_engine = 0
 	spaceship_trail_1.default_color = Color(0,255,255,1)
-	add_child(spaceship_trail_1)
+	add_child.call_deferred(spaceship_trail_1)
 	
 	var spaceship_trail_2 = preload(
 		"res://Entities/SpaceshipTrail/spaceship_trail.tscn").instantiate()
@@ -19,5 +29,5 @@ func _ready() -> void:
 	spaceship_trail_2.spaceship_engine = 1
 	spaceship_trail_2.default_color = Color(0,255,255,1)
 	
-	add_child(spaceship_trail_2)
+	add_child.call_deferred(spaceship_trail_2)
 	
