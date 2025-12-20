adac0954854bd361d639624bee7b646e55513930
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-12-20 05:57:29 -0500

    Removed explicit inputs

--- Entities/Spaceship/spaceship.gd
+++ Entities/Spaceship/spaceship.gd
@@ -2,18 +2,20 @@ extends Area2D
 
 var ROTATION_SPEED = PI
 var DEFAULT_SPEED = 750
-var controller_input_map = []
+var controller_input_map = {}
+
+func joy_axis_apply_deadzone(v:float, d:float)-> float:
+	if abs(v) >= d:
+		return v
+	else:
+		return 0.0
 
 func _physics_process(delta: float) -> void:
-	if len(controller_input_map) == 0:
-		return
+	var jv = Vector2(
+		joy_axis_apply_deadzone(Input.get_joy_axis(0, JOY_AXIS_LEFT_X), 0.2),
+		joy_axis_apply_deadzone(Input.get_joy_axis(0, JOY_AXIS_LEFT_Y), 0.2))
 
-	var jv = Input.get_vector(controller_input_map[0],
-	controller_input_map[1],
-	controller_input_map[2],
-	controller_input_map[3])
-	
-	if (jv.length() > 0): 
+	if (jv.length() > 0.2):
 		var d =  Vector2.UP.rotated(rotation).cross(jv.normalized())
 		if (d > 0.001):
 			rotation += ROTATION_SPEED * delta
@@ -21,7 +23,3 @@ func _physics_process(delta: float) -> void:
 			rotation -= ROTATION_SPEED * delta
 			
 	position +=  (Vector2.UP * DEFAULT_SPEED * delta).rotated(rotation)
-	
-	for a in controller_input_map:
-		if Input.is_action_just_pressed(a):
-			print(a)
--- Stages/FightZone/fight_zone.gd
+++ Stages/FightZone/fight_zone.gd
@@ -1,19 +1,10 @@
 extends Node2D
 
-var controller_inputs = [
-	["JA0LD0","JA0RD0","JA1UD0","JA1DD0","JB0D0",
-	"JB0D0","JB1D0","JB2D0","JB3D0",
-	"JB4D0","JB5D0","JB6D0","JB7D0",
-	"JB8D0","JB9D0","JB10D0","JB11D0",
-	"JB12D0","JB13D0","JB14D0","JB15D0",
-	"JB16D0","JB17D0","JB18D0","JB19D0",
-	"JB20D0"]]
 
 func _ready() -> void:
 	var spaceship = preload(
 		"res://Entities/Spaceship/spaceship.tscn").instantiate()
 	spaceship.position = Vector2(1920,1080)
-	spaceship.controller_input_map = controller_inputs[0]
 	add_child.call_deferred(spaceship)
 	
 	var spaceship_trail_1 = preload(
